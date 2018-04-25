3cc29fc901c616f93ddd44836c529a41401731a1
Unknown <George--Albert@hotmail.com>
2018-04-25 12:07:56 +0200

    Commit update PWM2 onto PWM1

--- FreeRTOS/FreeRTOS/src/main.c
+++ FreeRTOS/FreeRTOS/src/main.c
@@ -32,29 +32,8 @@
  * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
  */
 #include <asf.h>
-#include <inttypes.h>
-#include <stdio_serial.h>
-//Tasks
-#include "Tasks/task_player1.h"
-#include "Tasks/task_player2.h"
-#include "Tasks/task_control.h"
-//Delay
-#include "Utilities/DelayFunctions/delayFunctions.h"
-//Console Data Visualizer
-#include "Utilities/ConsoleFunctions/consoleFunctions.h"
-//FreeRTOS
-#include "FreeRTOS.h"
-//Semaphores
-#include "semphr.h"
-//Struct
-#include "xHandlerParameters.h"
-// From module: TC - Timer Counter
-#include "Utilities/TimerCounter/TimerCounter.h"
-//#include "PioInterrupt/PioInterrupt.h"
-
-#include "conf_board.h"
-#include "conf_clock.h"
 #include "MotorControl/MotorControl.h"
+#include "WheelCounters/WheelCounters.h"
 
 /**
  * \file
@@ -222,8 +201,9 @@ uint32_t events = pwm_channel_get_interrupt_status(PWM);
  * \return Unused (ANSI-C compatibility).
  */
 int main(void)
+int main (void)
 {
-	/* Initialize the SAM system */
+	// Insert system clock initialization code here (sysclk_init()).
 	sysclk_init();
 	board_init();
 	//initMotors();
@@ -321,6 +301,7 @@ pio_configure_pin(IOPORT_CREATE_PIN(PIOC, 22), PIO_TYPE_PIO_PERIPH_B);
 	NVIC_ClearPendingIRQ(PWM0_IRQn);
 	NVIC_SetPriority(PWM0_IRQn, 0);
 	NVIC_EnableIRQ(PWM0_IRQn);
+	// Insert application code here, after the board has been initialized.
 	
 	/* Enable PWM channels for LEDs */
 	pwm_channel_enable(PWM0, PIN_PWM_LED0_CHANNEL);
@@ -340,80 +321,6 @@ pio_configure_pin(IOPORT_CREATE_PIN(PIOC, 22), PIO_TYPE_PIO_PERIPH_B);
 	/* Infinite loop */
 	while (1) {
 	}
+	initMotors();
+	initSensors();
 }
-
-
-/*int main (void)
-{
-/*
-	//Instantiating the struct
-	xHandlerParameters* xHandler;
-	xHandler = ( xHandlerParameters* ) pvPortMalloc( sizeof( xHandler ) );
-	
-	// Insert board initialization code here (board_init())
-	board_init();
-	// Insert system clock initialization code here (sysclk_init())
-	sysclk_init();
-	// Configuring console (Tools->Data Visulizer)
-	configureConsole();
-	// Delay initialization
-	delayInit();
-	// Insert application code here, after the board has been initialized.
-	//init_timer();
-	ioport_init();
-	configPioInterrupt();	
-	//Check pin for the TC-handler
-	ioport_set_pin_dir(CHECK_PIN, IOPORT_DIR_OUTPUT);
-	//Configuring board settings
-	pmc_enable_periph_clk(ID_TRNG);
-	trng_enable(TRNG);
-	
-	
-	//analogInit(0);
-	
-	delayMicroseconds(100000);
-
-	// Initiating board pins
-	ioport_set_pin_dir(PIO_PB26_IDX, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_level(PIO_PB26_IDX, HIGH);
-	ioport_set_pin_dir(PIO_PA15_IDX, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_level(PIO_PA15_IDX, HIGH);
-	ioport_set_pin_dir(PIO_PC1_IDX, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_level(PIO_PA15_IDX, HIGH);
-	ioport_set_pin_dir(PIO_PC1_IDX, IOPORT_DIR_OUTPUT);
-	ioport_set_pin_level(PIO_PB21_IDX, HIGH);
-	
-	//Creating semaphores and tasks
-	xSemaphorePlayer1 = 0;
-	xSemaphorePlayer2 = 0;
-	
-	printf("Creating semaphores...\n");
-	vSemaphoreCreateBinary(xSemaphorePlayer1 );
-	vSemaphoreCreateBinary(xSemaphorePlayer2 );
-	
-	//Semaphores are taken and bot available
-	xSemaphoreTake(xSemaphorePlayer1, 0);
-	xSemaphoreTake(xSemaphorePlayer2, 0);
-
-	printf("Creating task handler...\n");
-	xTaskHandle taskHandlerPlayer1;
-	xTaskHandle taskHandlerPlayer2;
-
-	printf("Creating tasks...\n");
-	xTaskCreate(task_player1, (const signed char * const) "player1", TASK_PLAYER1_STACK_SIZE, NULL, TASK_PLAYER1_PRIORITY, &taskHandlerPlayer1);                     
-	xTaskCreate(task_player2, (const signed char * const) "player2", TASK_PLAYER2_STACK_SIZE, NULL, TASK_PLAYER2_PRIORITY, &taskHandlerPlayer2);
-	
-	printf("Task handler stored in struct xHandlerParameter.h...\n");
-	xHandler->taskplayer1 = &taskHandlerPlayer1;
-	xHandler->taskplayer2 = &taskHandlerPlayer2;
-	
-	/* Start the FreeRTOS scheduler running all tasks indefinitely*/    
-/*	printf("Starting scheduler...\n");
-	xTaskCreate(task_control, (const signed char * const) "control", TASK_CONTROL_STACK_SIZE, (void *) xHandler, TASK_CONTROL_PRIORITY, NULL);	
-	vTaskStartScheduler();
-	*/
-	//while(1);
-	
-	
-	
-/*}*/
