8a50412f878f5c168dc3584dd5df68926ea015b7
Unknown <George--Albert@hotmail.com>
2018-05-10 18:01:53 +0200

    Lag till conf_AdaLovelace.h från PWM2. Fattas filer fortfarande

---
+++ FreeRTOS/FreeRTOS/src/config/conf_AdaLovelace.h
@@ -0,0 +1,20 @@
+/*
+ * conf_AdaLovelace.h
+ *
+ * Created: 2018-05-01 11:39:36
+ *  Author: Sebastian Hoggard
+ */ 
+
+
+#ifndef CONF_ADALOVELACE_H_
+#define CONF_ADALOVELACE_H_
+
+#define SOFTDEC
+//#define HARDDEC
+
+//#define ISR_OVERRIDE
+
+#define REGULATION
+//#define NO_REGULATION
+
+#endif /* CONF_ADALOVELACE_H_ */
