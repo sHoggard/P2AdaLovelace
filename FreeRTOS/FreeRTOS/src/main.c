e14866e9d4946befcd0de5bb782587ff8bc11395
Unknown <George--Albert@hotmail.com>
2018-05-10 17:52:24 +0200

    kommenterat kod, ska överföra PWM2 main kod

--- FreeRTOS/FreeRTOS/src/main.c
+++ FreeRTOS/FreeRTOS/src/main.c
@@ -29,186 +29,191 @@
 
 
 
-int i = 0;
-
-float goalAngle = 0.0;
-float oldAngle = 0.0;
-float newAngle = 0.0;
-float angle = 0.0;
 
 
-//NAVIGATION MAIN
-int main (void)
-{
-	i++;
-	/* Initialize the SAM system */
-	sysclk_init();
-	board_init();
-	
-	delayInit();
-	//initMotors();
-	
-	//char buffer [34];
-	//itoa(sysclk_get_cpu_hz(), buffer, 10);
-	//puts(buffer);
-	
-	/* Configure the console uart for debug information */
-	configureConsole();
-	
-	
-	int test = 1;
-	while(test == 1){
-	/* -----------------------------------------------------------------------------------
-				NAVIGERING - TEST	
-	--------------------------------------------------------------------------------------*/
-	
-	//----------Test för att kontrollera så att grad-beräkning från radianer funkar----------
-	/*float degrees = 0.0;
-	float radians [] = {(PI/6), (PI/4), (PI/3), (PI/2), (PI), ((3*PI)/2), (2*PI)};
-	char rads [7][9] = {"PI/6 = ", "PI/4 = ", "PI/3 = ", "PI/2 = ", "PI = ", "3PI/2 = ", "2PI = "};
-	for (int i=0; i < 7; i++){
-		degrees = rad2deg(radians[i]);
-		printf(rads[i]);
-		printInt(degrees);
-	}*/
-	
-	
-	//----------Test för avståndsberäkningar----------
-	/*float distance = 0.0;
-	distance = distanceCalculation(2,2,2,5); //3 längdenheter
-	printf("Roboten i (2,5) har följande avstånd till objektet i (2,2) : ");
-	printInt(distance);
-	distance = distanceCalculation(2,4,2,2); //2 längdenheter
-	printf("\nRoboten i (4,2) har följande avstånd till objektet i (2,2) : ");
-	printInt(distance);
-	distance = distanceCalculation(2,2,2,2); //0 längdenheter
-	printf("\nRoboten i (2,2) har följande avstånd till objektet i (2,2) : ");
-	printInt(distance);
-	distance = distanceCalculation(2,10,2,2); //8 längdenheter
-	printf("\nRoboten i (10,2) har följande avstånd till objektet i (2,2) : ");
-	printInt(distance);*/
-	
-	
-	//----------Test för 1a kvadranten----------
-	/*goalAngle = degreeCalculation(2,1,2,2); //0 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,1,2,1);	//45 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,2,5,1);	//90 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,0,2,1);	//26grader
-	printInt(goalAngle);*/
-	
-	
-	//----------Test för 2a kvadranten----------
-	/*goalAngle = degreeCalculation(2,4,2,2);	//180 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,3,2,1);	//135 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,2,5,1);	//90 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,6,2,0);	//116 grader
-	printInt(goalAngle);*/
-	
-	
-	//----------Test för 3e kvadranten----------
-	/*goalAngle = degreeCalculation(2,4,2,3);	//243 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,2,2,5);	//270 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,3,1,5);	//194 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,5,2,5);	//225 grader
-	printInt(goalAngle);*/
-	
-	
-	//----------Test för 4e kvadranten----------
-	/*goalAngle = degreeCalculation(2,1,1,3);	//296 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,2,2,5);	//270 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(2,0,1,6);	//291 grader
-	printInt(goalAngle);
-	goalAngle = degreeCalculation(8,2,6,7);	//350 grader
-	printInt(goalAngle);*/
-	
-	
-	//----------Test för beräkning av korrigeringsvinkel----------
-	/*printf("\n----- TEST: ROTERA TILL 180 -----\n");
-	newAngle = correctionAngleCalculation(2,4,2,2,oldAngle);
-	angle = oldAngle + newAngle;
-	printf("Current angle: ");
-	printInt(oldAngle);
-	printf("\nGoal angle: ");
-	goalAngle = degreeCalculation(2,4,2,2);
-	printInt(goalAngle);	
-	printf("\nCorrection angle: ");
-	printInt(newAngle);
-	printf("\n--------------------------------\n");
-	oldAngle = angle;
-	
-	printf("\n----- TEST: ROTERA TILL 45 -----\n");
-	newAngle = correctionAngleCalculation(2,1,2,1,oldAngle);
-	angle = oldAngle + newAngle;
-	printf("Current angle: ");
-	printInt(oldAngle);
-	printf("\nGoal angle: ");
-	goalAngle = degreeCalculation(2,1,2,1);
-	printInt(goalAngle);
-	printf("\nCorrection angle: ");
-	printInt(newAngle);
-	printf("\n--------------------------------\n");
-	oldAngle = angle;
-	
-	printf("\n----- TEST: ROTERA TILL 0 -----\n");
-	newAngle = correctionAngleCalculation(2,1,2,2,oldAngle);
-	angle = oldAngle + newAngle;
-	printf("Current angle: ");
-	printInt(oldAngle);
-	printf("\nGoal angle: ");
-	goalAngle = degreeCalculation(2,1,2,2);
-	printInt(goalAngle);	
-	printf("\nCorrection angle: ");
-	printInt(newAngle);
-	printf("\n--------------------------------\n");
-	oldAngle = angle;
-	
-		printf("\n----- TEST: ROTERA TILL 225 -----\n");
-	newAngle = correctionAngleCalculation(2,5,2,5,oldAngle);
-	angle = oldAngle + newAngle;
-	printf("Current angle: ");
-	printInt(oldAngle);
-	printf("\nGoal angle: ");
-	goalAngle = degreeCalculation(2,5,2,5);
-	printInt(goalAngle);
-	printf("\nCorrection angle: ");
-	printInt(newAngle);
-	printf("\n--------------------------------\n");
-	oldAngle = angle;
-	
-	printf("\n----- TEST: ROTERA TILL 225 -----\n");
-	newAngle = correctionAngleCalculation(2,5,2,5,oldAngle);
-	angle = oldAngle + newAngle;
-	printf("Current angle: ");
-	printInt(oldAngle);
-	printf("\nGoal angle: ");
-	goalAngle = degreeCalculation(2,5,2,5);
-	printInt(goalAngle);
-	printf("\nCorrection angle: ");
-	printInt(newAngle);
-	printf("\n--------------------------------\n");
-	oldAngle = angle;*/
-	
-		
-	test = 0;
-	}
-	
-	/* Infinite loop */
-	while (1) 
-	{
-
-	}
-}
+//ctrk+k, ctrl+c fï¿½r att kommentera bort kod
+//ctrk+k, ctrl+u fï¿½r att avkommentera
+////2018-05-10
+//int i = 0;
+//
+//float goalAngle = 0.0;
+//float oldAngle = 0.0;
+//float newAngle = 0.0;
+//float angle = 0.0;
+//
+//
+////NAVIGATION MAIN
+//int main (void)
+//{
+	//i++;
+	///* Initialize the SAM system */
+	//sysclk_init();
+	//board_init();
+	//
+	//delayInit();
+	////initMotors();
+	//
+	////char buffer [34];
+	////itoa(sysclk_get_cpu_hz(), buffer, 10);
+	////puts(buffer);
+	//
+	///* Configure the console uart for debug information */
+	//configureConsole();
+	//
+	//
+	//int test = 1;
+	//while(test == 1){
+	///* -----------------------------------------------------------------------------------
+				//NAVIGERING - TEST	
+	//--------------------------------------------------------------------------------------*/
+	//
+	////----------Test för att kontrollera så att grad-beräkning från radianer funkar----------
+	///*float degrees = 0.0;
+	//float radians [] = {(PI/6), (PI/4), (PI/3), (PI/2), (PI), ((3*PI)/2), (2*PI)};
+	//char rads [7][9] = {"PI/6 = ", "PI/4 = ", "PI/3 = ", "PI/2 = ", "PI = ", "3PI/2 = ", "2PI = "};
+	//for (int i=0; i < 7; i++){
+		//degrees = rad2deg(radians[i]);
+		//printf(rads[i]);
+		//printInt(degrees);
+	//}*/
+	//
+	//
+	////----------Test för avståndsberäkningar----------
+	///*float distance = 0.0;
+	//distance = distanceCalculation(2,2,2,5); //3 längdenheter
+	//printf("Roboten i (2,5) har följande avstånd till objektet i (2,2) : ");
+	//printInt(distance);
+	//distance = distanceCalculation(2,4,2,2); //2 längdenheter
+	//printf("\nRoboten i (4,2) har följande avstånd till objektet i (2,2) : ");
+	//printInt(distance);
+	//distance = distanceCalculation(2,2,2,2); //0 längdenheter
+	//printf("\nRoboten i (2,2) har följande avstånd till objektet i (2,2) : ");
+	//printInt(distance);
+	//distance = distanceCalculation(2,10,2,2); //8 längdenheter
+	//printf("\nRoboten i (10,2) har följande avstånd till objektet i (2,2) : ");
+	//printInt(distance);*/
+	//
+	//
+	////----------Test för 1a kvadranten----------
+	///*goalAngle = degreeCalculation(2,1,2,2); //0 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,1,2,1);	//45 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,2,5,1);	//90 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,0,2,1);	//26grader
+	//printInt(goalAngle);*/
+	//
+	//
+	////----------Test för 2a kvadranten----------
+	///*goalAngle = degreeCalculation(2,4,2,2);	//180 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,3,2,1);	//135 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,2,5,1);	//90 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,6,2,0);	//116 grader
+	//printInt(goalAngle);*/
+	//
+	//
+	////----------Test för 3e kvadranten----------
+	///*goalAngle = degreeCalculation(2,4,2,3);	//243 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,2,2,5);	//270 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,3,1,5);	//194 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,5,2,5);	//225 grader
+	//printInt(goalAngle);*/
+	//
+	//
+	////----------Test för 4e kvadranten----------
+	///*goalAngle = degreeCalculation(2,1,1,3);	//296 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,2,2,5);	//270 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(2,0,1,6);	//291 grader
+	//printInt(goalAngle);
+	//goalAngle = degreeCalculation(8,2,6,7);	//350 grader
+	//printInt(goalAngle);*/
+	//
+	//
+	////----------Test för beräkning av korrigeringsvinkel----------
+	///*printf("\n----- TEST: ROTERA TILL 180 -----\n");
+	//newAngle = correctionAngleCalculation(2,4,2,2,oldAngle);
+	//angle = oldAngle + newAngle;
+	//printf("Current angle: ");
+	//printInt(oldAngle);
+	//printf("\nGoal angle: ");
+	//goalAngle = degreeCalculation(2,4,2,2);
+	//printInt(goalAngle);	
+	//printf("\nCorrection angle: ");
+	//printInt(newAngle);
+	//printf("\n--------------------------------\n");
+	//oldAngle = angle;
+	//
+	//printf("\n----- TEST: ROTERA TILL 45 -----\n");
+	//newAngle = correctionAngleCalculation(2,1,2,1,oldAngle);
+	//angle = oldAngle + newAngle;
+	//printf("Current angle: ");
+	//printInt(oldAngle);
+	//printf("\nGoal angle: ");
+	//goalAngle = degreeCalculation(2,1,2,1);
+	//printInt(goalAngle);
+	//printf("\nCorrection angle: ");
+	//printInt(newAngle);
+	//printf("\n--------------------------------\n");
+	//oldAngle = angle;
+	//
+	//printf("\n----- TEST: ROTERA TILL 0 -----\n");
+	//newAngle = correctionAngleCalculation(2,1,2,2,oldAngle);
+	//angle = oldAngle + newAngle;
+	//printf("Current angle: ");
+	//printInt(oldAngle);
+	//printf("\nGoal angle: ");
+	//goalAngle = degreeCalculation(2,1,2,2);
+	//printInt(goalAngle);	
+	//printf("\nCorrection angle: ");
+	//printInt(newAngle);
+	//printf("\n--------------------------------\n");
+	//oldAngle = angle;
+	//
+		//printf("\n----- TEST: ROTERA TILL 225 -----\n");
+	//newAngle = correctionAngleCalculation(2,5,2,5,oldAngle);
+	//angle = oldAngle + newAngle;
+	//printf("Current angle: ");
+	//printInt(oldAngle);
+	//printf("\nGoal angle: ");
+	//goalAngle = degreeCalculation(2,5,2,5);
+	//printInt(goalAngle);
+	//printf("\nCorrection angle: ");
+	//printInt(newAngle);
+	//printf("\n--------------------------------\n");
+	//oldAngle = angle;
+	//
+	//printf("\n----- TEST: ROTERA TILL 225 -----\n");
+	//newAngle = correctionAngleCalculation(2,5,2,5,oldAngle);
+	//angle = oldAngle + newAngle;
+	//printf("Current angle: ");
+	//printInt(oldAngle);
+	//printf("\nGoal angle: ");
+	//goalAngle = degreeCalculation(2,5,2,5);
+	//printInt(goalAngle);
+	//printf("\nCorrection angle: ");
+	//printInt(newAngle);
+	//printf("\n--------------------------------\n");
+	//oldAngle = angle;*/
+	//
+		//
+	//test = 0;
+	//}
+	//
+	///* Infinite loop */
+	//while (1) 
+	//{
+//
+	//}
+//}
 
 
 
