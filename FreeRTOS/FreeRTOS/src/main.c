36ae13de7a2ad084b291fc57cf6863dcb536426a
Unknown <George--Albert@hotmail.com>
2018-05-10 17:58:12 +0200

    PWM2 merge efter enhetstester av MotorControl. Main från PWM2. config ej

--- FreeRTOS/FreeRTOS/src/main.c
+++ FreeRTOS/FreeRTOS/src/main.c
@@ -23,10 +23,70 @@
 #include "Tasks/task_player2.h"
 #include "Tasks/task_control.h"
 //System modules
-#include "MotorControl/MotorControl.h"
 #include "WheelCounters/WheelCounters.h"
 #include "Navigering/angleCalculation.h"
+#include "config/conf_AdaLovelace.h"
+#include "MotorControl/MotorControl.h"
+#include "WheelCounters/WheelCounters.h"
+#include "Movement/Movement.h"
+#include "TimeTick/time_tick.h"
+//Config file
+#include "Movement/__vars.h"
 
+//PWM2 MAIN
+//2018-05-10
+char clrCom[] = {27, '[', '2', 'J', 27, '[', 'H', '\0'};
+
+
+int main (void)
+{
+	// Insert system clock initialization code here (sysclk_init()).
+	sysclk_init();
+	board_init();
+	configureConsole();
+
+	// Insert application code here, after the board has been initialized.
+	
+	initMovement();
+	
+	time_tick_init();
+	
+	char buffer[20];
+	
+	int speed = 100;
+	
+	scanf("%i", &speed);
+	
+	drive(speed, 0);
+	
+	printf("speed: %i\n\n", speed);
+	
+	int counter = 0;
+	
+	while (1)
+	{
+		if (time_tick_get()%100 == 0)
+		{
+			//fputs(clrCom, stdout);
+			//puts(itoa(counterLeft, buffer, 10));
+			//puts(itoa(counterRight, buffer, 10));
+			//puts(itoa(orientation(), buffer, 10));
+			//puts(itoa(regulated_speed.left, buffer, 10));
+			//puts(itoa(regulated_speed.right, buffer, 10));
+			//puts(itoa(regulated_speed.target, buffer, 10));
+			////drive(speed, 0);
+			//test_movement();
+			//if (speed < 500)
+			//{
+			//speed++;
+			//}
+			//delay_ms(1);
+			printf("%i: \n", counter++);
+			test_movement();
+			printf("\n");
+		}
+	}
+}
 
 
 
