27dd0764b4195590e6b26dc2cd74b47f0714636b
Unknown <info@bjornborisberggren.com>
2018-05-19 12:26:12 +0200

    TWI_Handler

--- FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/TWI_Handler.c
+++ FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/TWI_Handler.c
@@ -207,3 +207,19 @@ uint8_t communicationManager (uint8_t value)
 			
 		}
 	}
+	
+	uint8_t console_init(void)
+	{
+		const usart_serial_options_t uart_serial_options = {
+			.baudrate = CONF_UART_BAUDRATE,
+			.paritytype = CONF_UART_PARITY
+		};
+
+		// Configure console UART.
+		sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
+		stdio_serial_init(CONF_UART, &uart_serial_options);
+		
+		printf("Console ready\n");
+		printf("=============\n");
+		return 0;
+	}
--- FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/TWI_Handler.h
+++ FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/TWI_Handler.h
@@ -11,6 +11,7 @@
 #define SLAVE_ADDRESS_PABYGGNAD 8
 #define SLAVE_ADDRESS_POSITIONERING 0x10
 
+
 /* Commands to send to the slave */
 #define data_01					10
 #define data_02					20
@@ -24,8 +25,10 @@
 #define DATA_NOT_SENT 0
 #define DATA_NOT_READ 0
 #define RECEIVE_LENGTH_POSITIONERING		10	
+#define RECEIVE_LENGTH_PABYGGNAD			10
 
-uint8_t receive_data_positionering[RECEIVE_LENGTH_POSITIONERING];	
+uint8_t receive_data_positionering[RECEIVE_LENGTH_POSITIONERING];
+uint8_t receive_data_pabyggnad[RECEIVE_LENGTH_PABYGGNAD];	
 
 uint8_t init_twi(void);
 uint8_t transmit_package(uint8_t data, uint8_t slave);
@@ -33,5 +36,6 @@ uint8_t receive_package(uint8_t slave);
 uint8_t twi_send(uint8_t data_arr[], uint8_t data, twi_packet_t packet, uint8_t slave);
 uint8_t twi_read(uint8_t rec_data[], uint8_t data_length, twi_packet_t packet_rec);
 uint8_t communicationManager (uint8_t value);
+uint8_t console_init(void);
 
 #endif /* TWI_HANDLER_H_ */
