81ad160a17c2c71aa5741fcbe434834b319f81a3
Unknown <info@bjornborisberggren.com>
2018-05-29 19:01:56 +0200

    Testkod

--- FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/test_TWI.c
+++ FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/test_TWI.c
@@ -1,7 +1,7 @@
 /*
-* test.c
-*
-* Created: 2018-05-03 11:43:01
+* test_TWI.c
+* Author: Björn Berggren
+* Created: 2018-05-04 12:43:01
 *  
 */
 
@@ -18,11 +18,8 @@
 #define SERVO_STOP				5
 
 
-/************************************************************************/
-/*							  TEST TVÅ SLAVAR                           */
-/************************************************************************/
+// Påbyggnad
 
-/*
 void test_Led_On(void)
 {
 	transmit_package(LED_ON, SLAVE_ADDRESS_PABYGGNAD);
@@ -37,7 +34,7 @@ void test_Led_Off(void)
 
 void test_Servo_Right(void)
 {
-	transmit_package(SERVO_ROTATE_RIGHT, SLAVE_ADDRESS_PABYGGNAD)
+	transmit_package(SERVO_ROTATE_RIGHT, SLAVE_ADDRESS_PABYGGNAD);
 	delay_ms(3000);
 }
 
@@ -50,144 +47,10 @@ void test_Servo_Left(void)
 
 void test_Servo_Stop()
 {
-	printf("Rotate to The Stop");
+	printf("The Stop");
 	transmit_package(SERVO_STOP, SLAVE_ADDRESS_PABYGGNAD);
 	delay_ms(3000);
 }
 
-void test_fs_4()
-{
-	if(transmit_package(NULL,SLAVE_ADDRESS_POSITIONERING) == DATA_SENT)
-		printf("test_fs_4() = PASSED");
-	else
-		printf("test_fs_4() = NOT PASSED");
-}
-
-void test_fs_5()
-{
-	if(transmit_package(LED_ON,SLAVE_ADDRESS_PABYGGNAD) == DATA_SENT)
-		printf("test_fs_5() = NOT PASSED");
-	else
-		printf("test_fs_5() = PASSED");
-}
-
-void test_fs_6()
-{
-	if(transmit_package(LED_ON,SLAVE_ADDRESS_POSITIONERING) == DATA_SENT)
-		printf("test_fs_6() = NOT PASSED");
-	else
-		printf("test_fs_6() = PASSED");
-}
-
-void test_fs_7()
-{
-	if(transmit_package(LED_ON,SLAVE_ADDRESS_POSITIONERING) == DATA_SENT)
-		printf("test_fs_7() = PASSED");
-	else
-		printf("test_fs_7() = NOT PASSED");
-}
-
-void test_fs_8()
-{
-	if(transmit_package(LED_ON,SLAVE_ADDRESS_PABYGGNAD) == DATA_SENT)
-		printf("test_fs_8() = PASSED");
-	else
-		printf("test_fs_8() = NOT PASSED");
-}
-
-void test_fs_9()
-{
-	if (receive_package(SLAVE_ADDRESS_PABYGGNAD) && receive_package(SLAVE_ADDRESS_POSITIONERING))
-		printf("test_fs_9() = PASSED");
-	else
-		printf("test_fs_8() = NOT PASSED");
-}
-
-void test_fs_10(){
-	uint8_t counter =0;
-	while(counter<20)
-	{
-		transmit_package(data_01,SLAVE_ADDRESS_POSITIONERING);
-		counter++;
-	}
-}
 
 
-//
-//							  TEST EN SLAV                              
-/
-void test_s_1()
-{
-	if (transmit_package(LED_ON,SLAVE_ADDRESS_PABYGGNAD))
-	printf("test_fs_9() = PASSED");
-	else
-	printf("test_s_1() = NOT PASSED");
-}
-
-void test_s_2()
-{
-	uint8_t counter=0;
-	while(counter<20)
-	{
-		transmit_package(LED_ON,SLAVE_ADDRESS_PABYGGNAD);
-		delay_ms(3000);
-		transmit_package(LED_OFF,SLAVE_ADDRESS_PABYGGNAD);
-		delay_ms(3000);
-	}
-	printf("test_fs_9() = PASSED");
-}
-
-void test_s_3()
-{
-	if (transmit_package(SERVO_ROTATE_RIGHT,SLAVE_ADDRESS_PABYGGNAD)== DATA_SENT)
-		printf("test_s_3() = PASSED");
-	else
-		printf("test_s_3() = NOT PASSED");
-}
-
-void test_s_4()
-{
-	if (transmit_package(SERVO_ROTATE_LEFT,SLAVE_ADDRESS_PABYGGNAD)== DATA_SENT)
-		printf("%s", "test_s_4() = PASSED");
-	else
-		printf("test_s_4() = NOT PASSED");
-}
-
-void test_s_5()
-{
-	transmit_package(SERVO_ROTATE_RIGHT,SLAVE_ADDRESS_PABYGGNAD);
-	delay_ms(3000);
-	transmit_package(SERVO_STOP,SLAVE_ADDRESS_PABYGGNAD);
-}
-
-void test_s_6()
-{
-	if (receive_package(SLAVE_ADDRESS_PABYGGNAD)==DATA_READ)
-		printf("test_s_6() = PASSED");
-	else
-		printf("test_s_6() = NOT PASSED");
-}
-
-void test_s_7()
-{
-	if (receive_package(SLAVE_ADDRESS_PABYGGNAD)==DATA_READ)
-		printf("test_s_7() = PASSED");
-	else
-		printf("test_s_7() = NOT PASSED");
-}
-
-int console_init(void)
-{
-	const usart_serial_options_t uart_serial_options = {
-		.baudrate = CONF_UART_BAUDRATE,
-		.paritytype = CONF_UART_PARITY
-	};
-
-	// Configure console UART. 
-	sysclk_enable_peripheral_clock(CONSOLE_UART_ID);
-	stdio_serial_init(CONF_UART, &uart_serial_options);
-	
-	printf("Console ready\n");
-	printf("=============\n");
-	return 0;
-}*/
--- FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/test_TWI.h
+++ FreeRTOS/FreeRTOS/src/TWI_Kommunikationen/test_TWI.h
@@ -1,34 +1,22 @@
 /*
- * test.h
+ * test_TWI.h
  *
- * Created: 2018-05-03 11:43:14
+ *Author: Björn Berggren
+ * Created: 2018-05-04 12:42:11
  *
  */ 
 
 
-#ifndef TEST_H_
-#define TEST_H_
+#ifndef TEST_TWI_H_
+#define TEST_TWI_H_
 
-//int console_init(void);
 
-/* Olika testfall från testprotokollet "I2C - Flera slaves" */
+
+void test_Servo_Right(void);
 void test_Led_On(void);
 void test_Servo_Right(void);
-void test_fs_3(void);
-void test_fs_4(void);
-void test_fs_5(void);
-void test_fs_6(void);
-void test_fs_7(void);
-void test_fs_8(void);
-void test_fs_9(void);
-void test_fs_10(void);
-void test_s_1(void);
-void test_s_2(void);
-void test_s_3(void);
-void test_s_4(void);
-void test_s_5(void);
-void test_s_6(void);
-void test_s_7(void);
+void test_Servo_Left(void);
+void test_Servo_Stop(voidS);
 
 
 
